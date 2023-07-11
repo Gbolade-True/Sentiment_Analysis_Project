81a1bfb0309106e01697e70d2691ec84a9b4d871
Gbolade-True <ibukunoluwa.g.popoola@gmail.com>
2023-07-11 18:58:08 +0100

    - further mobile modal fixes

--- src/components/landing/results/index.tsx
+++ src/components/landing/results/index.tsx
@@ -38,7 +38,6 @@ export const Results = ({ loading, predictions }: ResultProps) => {
                 />
             return ResultsDisplay
         }
-
         ResultsDisplay = 
             <>
                 {
